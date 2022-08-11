fc5fe07c29c7666b4c852783e8236533bc39f366
Diego Iaconelli <matdotcx@github.com>
2022-08-11 15:19:56 +0100

    adding xcode and macports automation

---
+++ macports.sh
@@ -0,0 +1,16 @@
+#!/bin/zsh
+# Installing MacPorts from source
+# Create a working directory to build from
+echo "Creating MacPorts source location"
+mkdir -p /opt/mports
+cd /opt/mports
+# Clone from the project repo
+git clone https://github.com/macports/macports-base.git
+cd macports-base
+# Checkout the current version and build / install / clean to `/opt/local`
+git checkout
+echo "Building MacPorts"
+./configure --enable-readline
+make
+sudo make install
+make distclean
