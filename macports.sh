9f7aa32438fd359328564f094fad04fc5d1ab141
Diego Iaconelli <matdotcx@github.com>
2022-10-18 21:28:21 +0100

    Update macports.sh
    Repairing macports.sh permissions

--- macports.sh
+++ macports.sh
@@ -4,9 +4,15 @@
 # Source; https://github.com/matdotcx/
 #########################################################
 
+# Ask for the administrator password upfront
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until we have finished
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
 # Create a working directory to build from
 echo "Creating MacPorts source location"
-mkdir -p /opt/mports
+sudo mkdir -p /opt/mports
 cd /opt/mports
 # Clone from the project repo
 git clone https://github.com/macports/macports-base.git
