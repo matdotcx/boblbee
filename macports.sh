9b133cf812457500966999dc701f799a8f491e58
Diego Iaconelli <diego@iaconelli.org>
2023-01-17 12:00:08 +0000

    blorp

--- macports.sh
+++ macports.sh
@@ -7,16 +7,26 @@
 # Ask for the administrator password upfront
 sudo -v
 
-# Keep-alive: update existing `sudo` time stamp until we have finished
-while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+# Define timestamp variable
+timestamp=$(date +%d-%m-%Y_%H.%M.%S)
 
-# Create a working directory to build from
-echo "Creating MacPorts source location"
-sudo mkdir -p /opt/mports
+#########################################################
+
+# Check if the mports folder exists in /opt/
+if [ -d "/opt/mports" ]; then
+  # If it does, tar and move the folder, then delete the original
+  tar -cvzf /opt/mports$timestamp.tar.gz /opt/mports/
+  mv /opt/mports$timestamp.tar.gz /opt/
+  rm -rf /opt/mports
+fi
+
+# Create a new mports folder in /opt/ and cd into it
+mkdir /opt/mports
 cd /opt/mports
-# Clone from the project repo
-git clone https://github.com/macports/macports-base.git
-cd macports-base
+
+# Clone the macports-base repo
+git clone https://github.com/macports/macports-base.git && cd macports-base
+
 # Checkout the current version and build / install / clean to `/opt/local`
 git checkout
 echo "Building MacPorts"
