834722bb1590c28cedb2bc5483662512ad6e0e2a
Diego Iaconelli <diego@iaconelli.org>
2023-07-20 15:43:40 +0100

    Update index.sh
    Doing the things as superuser

--- scripts/index.sh
+++ scripts/index.sh
@@ -4,8 +4,14 @@
 # Source; https://github.com/matdotcx/
 #########################################################
 
+# Ask for the administrator password upfront
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until we have finished
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
 # Run each program
-sh "touchid-sudo.sh"
-sh "xcode.sh"
-sh "macports.sh"
-sh "defaults.sh"
+sh sudo "touchid-sudo.sh"
+sh sudo "xcode.sh"
+sh sudo "macports.sh"
+sh sudo "defaults.sh"
