189729d61993d3ced63a56fadb8081a541b4c012
matdotcx <matdotcx@users.noreply.github.com>
2023-08-17 15:16:18 +0100

    Adding runAsUser

--- scripts/macports.sh
+++ scripts/macports.sh
@@ -8,11 +8,45 @@
 # Ask for the administrator password upfront
 sudo -v
 
+#########################################################
 # Keep-alive: update existing `sudo` time stamp until we have finished
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
+#########################################################
 # Define timestamp variable
 timestamp=$(date +%d-%m-%Y_%H.%M.%S)
+
+#########################################################
+# runAsUser courtesy of Armin Briegel - Scripting OS X
+
+export PATH=/usr/bin:/bin:/usr/sbin:/sbin
+
+# get the currently logged in user
+currentUser=$( echo "show State:/Users/ConsoleUser" | scutil | awk '/Name :/ { print $3 }' )
+
+# global check if there is a user logged in
+if [ -z "$currentUser" -o "$currentUser" = "loginwindow" ]; then
+  echo "no user logged in, cannot proceed"
+  exit 1
+fi
+# now we know a user is logged in
+
+# get the current user's UID
+uid=$(id -u "$currentUser")
+
+# convenience function to run a command as the current user
+# usage:
+#   runAsUser command arguments...
+runAsUser() {  
+  if [ "$currentUser" != "loginwindow" ]; then
+    launchctl asuser "$uid" sudo -u "$currentUser" "$@"
+  else
+    echo "no user logged in"
+    # uncomment the exit command
+    # to make the function exit with an error when no user is logged in
+    # exit 1
+  fi
+}
 
 #########################################################
 
@@ -46,4 +80,4 @@ make distclean
 
 echo ""
 echo "Adding /opt/local as a local path"
-export PATH=/opt/local/bin:/opt/local/sbin:$PATH
+runAsUser export PATH=/opt/local/bin:/opt/local/sbin:$PATH
